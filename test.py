c013970b1cb77b5037c51b6dc7ba4083c69eb145
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-11-30 09:24:36 +0700

    feat: [GEC] Control using UART

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ import cv2
 
 model = PlankModel()
 
-image_path = "test.jpg"
+image_path = "/home/nhutdeptrai/NCKH/gym-exercise-correction-tiny/test.jpg"
 
 image = cv2.imread(image_path)
 
 result_image = model.predict(frame=image, prediction_probability_threshold=0.5)
 
-cv2.imshow("Result", result_image)
-cv2.waitKey(0)
+cv2.imwrite("test_result.jpg", result_image)
